e547a2e49256b91af1a70bb6e590baf2a70521de
Azzaid <Johanas.Azzaid@gmail.com>
2018-05-07 16:02:42 +0300

    Started to style app. Firstly deleted all custom styles.

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { createMuiTheme } from 'material-ui/styles';
+import deepPurple from 'material-ui/colors/deepPurple';
 import SortableTable from './SortableTable';
 import SearchField from './SearchField';
 
@@ -11,9 +13,18 @@ const columnData = [
   { id: 'year', disablePadding: false, alignTooltipToLeft:true, label: 'Год' },
 ];
 
+
+
 export default class App extends Component {
   constructor (props){
     super(props);
+    this.mainTheme = createMuiTheme({
+      palete:{
+        primary:{
+          main:'#ff4400',
+        },
+      },
+    });
     this.state = {
       data: props.data
     };
@@ -32,17 +43,9 @@ export default class App extends Component {
   render() {
     console.log("app state in render is:", this.state);
     return (
-      <MuiThemeProvider>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <MuiThemeProvider theme={this.mainTheme}>
         <SortableTable data = {this.state.data} columns={columnData} />
         <SearchField search={this.requestData}/>
-      </div>
       </MuiThemeProvider>
     );
   }
